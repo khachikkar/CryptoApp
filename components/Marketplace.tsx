c8aced5da2317bef9e87c224a958827a81a31492
khachikkar <khachikkarapetyan78@gmail.com>
2024-12-25 01:08:43 +0400

    delated unnessasary data

--- components/Marketplace.tsx
+++ components/Marketplace.tsx
@@ -5,22 +5,6 @@ import {supabase} from "@/service/supabase";
 import {useEffect, useState} from "react";
 
 
-
-
-
-const nftItems = [
-  { id: 1, name: 'Cosmic Cube', price: '0.5 ETH', image: one },
-  { id: 2, name: 'Digital Dreamscape', price: '0.8 ETH', image: two },
-  { id: 3, name: 'Neon Nexus', price: '0.3 ETH', image: tree },
-  { id: 4, name: 'Quantum Quasar', price: '1.2 ETH', image: four },
-]
-
-
-
-
-
-
-
 export default function Marketplace() {
 
     type DataType = {
@@ -69,7 +53,7 @@ export default function Marketplace() {
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-12">NFT Marketplace Preview</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-            {mdata.map((item) => (
+            {mdata && ( mdata.map((item) => (
                 <div key={item.id} className=" bg-gray-700 bg-opacity-30 rounded-lg overflow-hidden">
                   <Image
                       src={item.image_url}
@@ -83,7 +67,8 @@ export default function Marketplace() {
                     <p className="text-purple-400">{item.price} ETH</p>
                   </div>
                 </div>
-            ))}
+            )))}
+              No Data
           </div>
         </div>
       </section>
